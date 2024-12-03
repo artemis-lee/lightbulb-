139e63f3b850c48e55edeb997143dced0de6c668
Artem <artem.23.cz@gmail.com>
2024-12-03 15:02:07 +0900

    last_commit

--- script.js
+++ script.js
@@ -4,12 +4,12 @@
     image.addEventListener('click', ((e) => {
         console.log (e.target);
         
-        if (e.target.getAttribute ('src') === '/lightbulb-solid-off.svg' ) {
-            e.target.setAttribute ('src', "/lightbulb-solid-on.svg");
+        if (e.target.getAttribute ('src') === 'lightbulb-solid-off.svg' ) {
+            e.target.setAttribute ('src', "lightbulb-solid-on.svg");
             document.body.style.backgroundColor = '#1f1768';
         }
         else {
-            e.target.setAttribute ('src', "/lightbulb-solid-off.svg");
+            e.target.setAttribute ('src', "lightbulb-solid-off.svg");
             document.body.style.backgroundColor = '#fefee7';
         }
     }))
